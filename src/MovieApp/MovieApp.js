62165ea3e8815301193901b4e87216f752fae2af
hoyeol0513 <hoyeol0513@naver.com>
2022-07-29 03:36:40 +0900

    파일 정리 및 MovieApp router와 component 추가 및 정리

--- src/MovieApp/MovieApp.js
+++ src/MovieApp/MovieApp.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function MovieApp(){
   const [loading, setLoading] = useState(true);
@@ -21,19 +22,15 @@ function MovieApp(){
     <h1>Loading...</h1>
   ) : (
     <div>
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <img src={movie.medium_cover_image} />
-          <h2>{movie.title}</h2>
-          {/* p = paragraph */}
-          <p>{movie.summary}</p>
-          <ul>
-            {movie.genres.map((g)=>(
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
-        </div>
-        ))}
+      {movies.map((movie) =>(
+        <Movie 
+        key={movie.id}
+        coverImg={movie.medium_cover_image}
+        title={movie.title}
+        summary={movie.summary}
+        genres={movie.genres} 
+        /> 
+      ))}
         </div>
     )}
   </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './MovieApp';
+import App from './MovieApp/MovieApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
